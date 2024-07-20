029ab5e93f6f468a4b0a8c5a6376c28bbdcf8389
Sanjanah8 <139737652+Sanjanah8@users.noreply.github.com>
2024-07-20 13:11:10 +0530

    Create about us.js

---
+++ about us.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutSection = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>PetMe is a platform dedicated to connecting pet lovers with adoptable pets in their area. Our mission is to help pets find loving homes and provide resources for pet owners.</p>
+        </div>
+    );
+};
+
+export default AboutSection;
